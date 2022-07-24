17c929f7b0a451d029f9fbe5bdd50e1c9a00bea1
CzBiX <CzBiX@users.noreply.github.com>
2022-07-24 12:55:31 +0800

    Tweak copy button style

--- playground/src/components/ui/PanelToolbar.vue
+++ playground/src/components/ui/PanelToolbar.vue
@@ -2,7 +2,8 @@
   <div
     class="flex absolute top-0 right-0
       bg-dark-300 p-2 rounded-lb-2 gap-2
-      opacity-20 hover:opacity-80 transition-opacity"
+      opacity-20 hover:opacity-80 transition-opacity
+      children:(color-neutral-500 transition-color p-3) hover:children:color-neutral-100"
   >
     <slot />
   </div>
